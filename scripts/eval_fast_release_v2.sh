362396b8a0ca4035598c3d5f738192bb5fbd8f04
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-14 13:35:21 -0300

    Update paths

--- scripts/eval_fast_release_v2.sh
+++ scripts/eval_fast_release_v2.sh
@@ -1,10 +1,11 @@
 GPU_ID=$1
 outf_path=val_release_v2
-sep_full_path=../../data/models/dynamic_models/box_only_attrV3.pth
-eval_full_path=../../data/models/dynamic_models/attrV3_offset4.pth
-CLEVRER_ANN_PATH=../../data/raw_data/clevrer
-TUBE_PRP_PATH=../../data/raw_data/train_val_proposals 
-CUDA_VISIBLE_DEVICES=${GPU_ID} python eval_tube_sep.py \
+sep_full_path=/datasets/dcl_clevrer/models/box_only_attrV3.pth
+eval_full_path=/datasets/dcl_clevrer/models/attrV3_offset4.pth
+CLEVRER_ANN_PATH=/datasets/dcl_clevrer
+TUBE_PRP_PATH=/datasets/dcl_clevrer/models/tubeProposalsAttrMatchTest/1.0_1.0_0.6_0.7
+CUDA_VISIBLE_DEVICES=${GPU_ID}
+python3 eval_tube_sep.py \
     --store_img 1 \
     --tube_mode 1 \
     --edge_superv 0 \
