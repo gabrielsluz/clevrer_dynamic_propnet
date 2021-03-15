e8fed5070357ef2287bd9dcd65ef71b85c5e6cba
gabrielsluz <gabrielsluz1999@gmail.com>
2021-03-14 22:38:59 -0300

    Resume and send to server

--- scripts/eval_fast_release_v2.sh
+++ scripts/eval_fast_release_v2.sh
@@ -11,23 +11,23 @@ python3 eval_tube_sep.py \
     --edge_superv 0 \
     --use_attr 0 \
     --store_patch_flag 1 \
-    --st_idx 15000 \
+    --st_idx 18497 \
     --ed_idx 20000 \
     --box_only_flag 0 \
     --state_dim 7 \
     --relation_dim 5 \
     --new_mode 0 \
     --frame_offset 5 \
-    --outf dumps/${outf_path} \
-    --des_dir dumps/annos/${outf_path}_separate_realOffset5 \
-    --evalf dumps/visualization/${outf_path}_separate_realOffset5 \
+    --outf /datasets/dcl_clevrer/dynamic_propnet/${outf_path} \
+    --des_dir /datasets/dcl_clevrer/dynamic_propnet/annos/${outf_path}_separate_realOffset5 \
+    --evalf /datasets/dcl_clevrer/dynamic_propnet/visualization/${outf_path}_separate_realOffset5 \
     --epoch 0 \
     --iter 0 \
     --state_dim_spatial 4 \
     --relation_dim_spatial 3 \
     --separate_mode 1 \
     --maskout_pixel_inference_flag 1 \
-    --des_dir dumps/propnet_predictions_prpAttrV3 \
+    --des_dir /datasets/dcl_clevrer/dynamic_propnet/propnet_predictions_prpAttrV3 \
     --data_dir ${CLEVRER_ANN_PATH} \
     --prp_dir ${CLEVRER_ANN_PATH}/proposals \
     --ann_dir ${CLEVRER_ANN_PATH} \
